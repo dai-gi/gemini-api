a29471945cbea6cd8ba9a17e64bbfe8600a1984d
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2024-07-26 08:24:02 +0900

    langchainを使ったチャットアプリを作成

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model='gemini-pro')
+
+st.title('langchain-streamlit-app')
+
+if 'messages' not in st.session_state:
+  st.session_state.messages = []
+
+for message in st.session_state.messages:
+  with st.chat_message(message['role']):
+    st.markdown(message['content'])
+
+prompt = st.chat_input('What is up?')
+
+if prompt:
+  st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+  with st.chat_message('user'):
+    st.markdown(prompt)
+  with st.chat_message('assistant'):
+    response = llm.invoke(prompt)
+    st.markdown(response.content)
+
+  st.session_state.messages.append({'role': 'assistant', 'content': response.content})
