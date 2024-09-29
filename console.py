a90ab0d0db012036ccbf65921f091b8b6ec7d11a
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2024-09-29 11:22:04 +0900

    空白追加、不要な行の削除

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 load_dotenv()
 
-GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel("gemini-pro")
--- gemini.py
+++ gemini.py
@@ -5,9 +5,11 @@ import google.generativeai as genai
 load_dotenv()
 
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+# GOOGLE_API_KEY = 'AIzaSyDctvtsI1RXWE1hEETzSvjncO3Q8Idbe4U'
 genai.configure(api_key=GOOGLE_API_KEY)
 
 gemini_pro = genai.GenerativeModel("gemini-pro")
 prompt  = "こんにちは"
 response = gemini_pro.generate_content(prompt)
 print(response.text)
+print('終了')
